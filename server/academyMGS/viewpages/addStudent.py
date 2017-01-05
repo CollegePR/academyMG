768f85f48ff32938ae8c73c2ba3f6360e90f31b6
jungsungwook <jswcyber@naver.com>
2017-01-05 19:46:43 +0900

    17-01-05-2차 점검
    아직 안한것 : 이미지와 날짜만추가하면됨
    고친것 : 트라이캐치문 고침

--- server/academyMGS/viewpages/addStudent.py
+++ server/academyMGS/viewpages/addStudent.py
@@ -2,35 +2,50 @@ from academyMGS.models import *
 import json
 from django.http import HttpResponse
 
+
 def addStudent(request):
-    data = [{'status':False},{'flag',False}]
-    id=""
-    name=""
-    sex=""
-    phone_num=""
-    address=""
-    school_name=""
-    grade=""
-    school_class=""
+    data = [{'status': False}, {'flag': False}]
+    requestId = ""
+    name = ""
+    sex = ""
+    phone_num = ""
+    address = ""
+    school_name = ""
+    grade = ""
+    school_class = ""
     try:
         if request.method == "POST":
-            id = request.POST['id']
-            name = request.POST['name']
-            sex = request.POST['sex']
-            phone_num = request.POST['phone_num']
-            address = request.POST['address']
-            school_name = request.POST['school_name']
-            school_class = request.POST['school_class']
+            requestId = request.POST.get('requestid')
+
+            name = request.POST.get('name')
+
+            sex = request.POST.get('sex')
+
+            phone_num = request.POST.get('phone_num')
+
+            address = request.POST.get('address')
+
+            school_name = request.POST.get('school_name')
+
+            school_class = request.POST.get('school_class')
+
 
 
         else:
+            print("try문 else문진입")
             return HttpResponse(json.dumps(data), content_type='application/json')
 
-        st=Student( id = id, name = name , sex = sex , phone_num = phone_num , address = address , school_name = school_name,school_class=school_class )
+        st = Student(id=requestId, name=name,
+                     sex=sex,
+                     phone_num=phone_num,
+                     address=address,
+                     school_name=school_name,
+                     school_class=school_class
+                     )
         st.save()
-        data = [{'status': True},{'flag':True}]
-
+        data = [{'status': True}, {'flag': True}]
     except:
+        print("예외문진입")
         return HttpResponse(json.dumps(data), content_type='application/json')
 
     return HttpResponse(json.dumps(data), content_type='application/json')
