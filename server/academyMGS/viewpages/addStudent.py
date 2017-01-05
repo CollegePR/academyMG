38a4e8b0ce6aa97d19628f0a653584bad8a173b7
jungsungwook <jswcyber@naver.com>
2017-01-05 21:45:46 +0900

    17-01-05 마지막 점검
    추가해야할 것은 이제 이미지등 더미파일 박아보기~

--- server/academyMGS/viewpages/addStudent.py
+++ server/academyMGS/viewpages/addStudent.py
@@ -4,46 +4,55 @@ from django.http import HttpResponse
 
 
 def addStudent(request):
-    data = [{'status': False}, {'flag': False}]
-    requestId = ""
+    data = [{'status': False, 'flag': False}]
     name = ""
-    sex = ""
+    sex = True
     phone_num = ""
     address = ""
     school_name = ""
-    grade = ""
-    school_class = ""
+    grade = 0
+    school_class = 0
+    status_of_sign = 2
+
+
     try:
         if request.method == "POST":
-            requestId = request.POST.get('requestid')
-
             name = request.POST.get('name')
-
             sex = request.POST.get('sex')
-
             phone_num = request.POST.get('phone_num')
-
             address = request.POST.get('address')
-
             school_name = request.POST.get('school_name')
-
+            grade = request.POST.get('grade')
             school_class = request.POST.get('school_class')
-
+            status_of_sign = request.POST.get('status_of_sign')
+            print(name)
+            print(sex)
+            print(phone_num)
+            print(address)
+            print(school_name)
+            print (school_class)
+            print (grade)
+            print (status_of_sign)
 
 
         else:
             print("try문 else문진입")
             return HttpResponse(json.dumps(data), content_type='application/json')
 
-        st = Student(id=requestId, name=name,
-                     sex=sex,
-                     phone_num=phone_num,
-                     address=address,
-                     school_name=school_name,
-                     school_class=school_class
-                     )
-        st.save()
-        data = [{'status': True}, {'flag': True}]
+        student = Student(
+            name=name,
+            sex=sex,
+            phone_num=phone_num,
+            address=address,
+            school_name=school_name,
+            school_class=school_class,
+            grade=grade,
+            status_of_sign=status_of_sign,
+
+
+        )
+        student.save()
+        data = [{'status': True, 'flag': True}]
     except:
         print("예외문진입")
         return HttpResponse(json.dumps(data), content_type='application/json')
