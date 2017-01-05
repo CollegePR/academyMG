f217564574a32bbeee970dcb6475c7f8a826edf4
zaeval <h3065@naver.com>
2017-01-05 16:40:04 +0900

    [api] register success
    register 완료

--- server/academyMGS/viewpages/register.py
+++ server/academyMGS/viewpages/register.py
@@ -2,26 +2,31 @@ from academyMGS.models import *
 import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-def register(request):
-    data = [{'status':False},{'flag',False}]
-    id=""
-    password=""
-    academy_class=""
-    name=""
-    if request.method == "POST":
-        id=request.POST['id'];
-        password=request.POST['password'];
-        academy_class=request.POST['class'];
-        name=request.POST['name'];
-    else:
-        return HttpResponse(json.dumps(data), content_type='application/json')
-    teacher = Teacher(
-        id=id,
-        password=password,
-        acdemy_class=academy_class,
-        name=name,
-    )
-    teacher.save()
-    data = [{'status': True},{'flag':True}]
-    return HttpResponse(json.dumps(data), content_type='application/json')
 
+
+def register(request):
+    data = [{'status': False,'flag': False}]
+    id = ""
+    password = ""
+    academy_class = ""
+    name = ""
+    try:
+        if request.method == "POST":
+            id = request.POST['id'];
+            password = request.POST['password'];
+            academy_class = request.POST['class'];
+            name = request.POST['name'];
+        else:
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        teacher = Teacher(
+            id=id,
+            password=password,
+            acdemy_class=academy_class,
+            name=name,
+        )
+        teacher.save()
+        data = [{'status': True, 'flag': True}, ]
+    except TypeError:
+        return HttpResponse(json.dumps(data), content_type='application/json')
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
