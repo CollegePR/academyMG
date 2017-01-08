c2225a916d9bd67c5dfe1adaf980302447e8211a
YKR-PROMETA <okms00909@naver.com>
2017-01-09 04:38:08 +0900

    Notification및 다이얼로그폼 추가

---
+++ client/c#/AcademyMG-Client/MaterialSkinExample/Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using ToastNotifications;
+using MaterialSkinExample.ShowDialog;
+
+namespace MaterialSkinExample
+{
+    public static class Util
+    {
+        public static void ShowInDialog(string Title, string Text)
+        {
+            ShowDialogForm showDialogForm = new ShowDialogForm(Title, Text);
+            showDialogForm.ShowDialog();
+        }
+
+        public static void ShowInDialog(string Text)
+        {
+            ShowDialogForm showDialogForm = new ShowDialogForm(Text);
+            showDialogForm.ShowDialog();
+        }
+
+        public static void Notification(string Name, string Text)
+        {
+            Notification NotifyForm = new Notification(Name, Text, 5, FormAnimator.AnimationMethod.Slide, FormAnimator.AnimationDirection.Up);
+            NotifyForm.Show();
+        }
+
+        public static void Notification(string Text)
+        {
+            Notification NotifyForm = new Notification("", Text, 5, FormAnimator.AnimationMethod.Slide, FormAnimator.AnimationDirection.Up);
+            NotifyForm.Show();
+        }
+    }
+}
