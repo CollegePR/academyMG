8e8dc061dcdef4681bc3cce3e7c29b48be286dda
zaeval <h3065@naver.com>
2017-01-08 22:58:56 +0900

    [api] modify register.py & accessList.py
    register.py & accessList.py 수정 & 테스트 디비 넣었음.

--- server/academyMGS/viewpages/accessList.py
+++ server/academyMGS/viewpages/accessList.py
@@ -1,23 +1,22 @@
 from academyMGS.models import *
 import json
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
 
 def accessList(request):
     data = {'flag': False, 'data': []}
     requestId = ""
     try:
+
         objs = Teacher.objects.filter(status=1)
         for obj in objs:
-            data.get("data").insert(
+            data.get("data").insert(0,
                 {
                     "name": obj.name,
                     "id": obj.id,
                     "academyClass": obj.acdemy_class,
                 }
             )
+        data["flag"]=True
     except:
         return HttpResponse(json.dumps(data), content_type='application/json')
-
     return HttpResponse(json.dumps(data), content_type='application/json')
--- server/academyMGS/viewpages/register.py
+++ server/academyMGS/viewpages/register.py
@@ -10,23 +10,26 @@ def register(request):
     password = ""
     academy_class = ""
     name = ""
-    if request.method == "POST":
-        id = request.POST.get('id');
-        password = request.POST.get('password');
-        academy_class = request.POST.get('academy_class');
-        name = request.POST.get('name');
-    else:
+    try:
+        if request.method == "POST":
+            id = request.POST.get('id');
+            password = request.POST.get('password');
+            academy_class = request.POST.get('academy_class');
+            name = request.POST.get('name');
+        else:
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        if(Teacher.objects.filter(id=id).count()!=0):
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        teacher = Teacher(
+            id=id,
+            password=password,
+            acdemy_class=academy_class,
+            name=name,
+            status=1,
+        )
+        teacher.save()
+        data = {'flag': True}
+    except:
         return HttpResponse(json.dumps(data), content_type='application/json')
-    teacher = Teacher(
-        id=id,
-        password=password,
-        acdemy_class=academy_class,
-        name=name,
-        status=1,
-    )
-
-    teacher.save()
-    data = {'flag': True}
-
 
     return HttpResponse(json.dumps(data), content_type='application/json')
