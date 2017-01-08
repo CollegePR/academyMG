1dd4fa64ec113cb953bd9bf429552c932f03066a
박정현 <pjh9996@naver.com>
2017-01-08 21:56:51 +0900

    클라이언트 틀 수정 & 테스트코드
    .

---
+++ client/c#/AcademyMG-Client/MaterialSkinExample/RetrofitService/LoginData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialSkinExample.RetrofitService
+{
+    public class LoginData
+    {
+        public string id;
+        public string password;
+        public bool flag;
+    }
+}
