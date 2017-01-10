495ee072cf4a59e9e7d29551e972acddef8fcbf6
박정현 <pjh9996@naver.com>
2017-01-10 19:27:01 +0900

    [Client] LoginForm Start
    클라 로그인 파트 시작

---
+++ client/c#/AcademyMG/MaterialSkinExample/LoginRegister/LoginForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MaterialSkinExample.LoginRegister
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RegisterButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.SubmitButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.tf_login_pw = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.tf_login_id = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.AutoSize = true;
+            this.RegisterButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.RegisterButton.Depth = 0;
+            this.RegisterButton.Icon = null;
+            this.RegisterButton.Location = new System.Drawing.Point(131, 200);
+            this.RegisterButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RegisterButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Primary = true;
+            this.RegisterButton.Size = new System.Drawing.Size(83, 36);
+            this.RegisterButton.TabIndex = 7;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.AutoSize = true;
+            this.SubmitButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.SubmitButton.Depth = 0;
+            this.SubmitButton.Icon = null;
+            this.SubmitButton.Location = new System.Drawing.Point(33, 200);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SubmitButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Padding = new System.Windows.Forms.Padding(18, 16, 18, 16);
+            this.SubmitButton.Primary = true;
+            this.SubmitButton.Size = new System.Drawing.Size(71, 36);
+            this.SubmitButton.TabIndex = 6;
+            this.SubmitButton.Text = "SUBMIT";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // tf_login_pw
+            // 
+            this.tf_login_pw.Depth = 0;
+            this.tf_login_pw.Hint = "Password";
+            this.tf_login_pw.Location = new System.Drawing.Point(33, 136);
+            this.tf_login_pw.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tf_login_pw.MaxLength = 32767;
+            this.tf_login_pw.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tf_login_pw.Name = "tf_login_pw";
+            this.tf_login_pw.PasswordChar = '*';
+            this.tf_login_pw.SelectedText = "";
+            this.tf_login_pw.SelectionLength = 0;
+            this.tf_login_pw.SelectionStart = 0;
+            this.tf_login_pw.Size = new System.Drawing.Size(181, 23);
+            this.tf_login_pw.TabIndex = 5;
+            this.tf_login_pw.TabStop = false;
+            this.tf_login_pw.UseSystemPasswordChar = false;
+            this.tf_login_pw.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tf_login_pw_KeyDown);
+            // 
+            // tf_login_id
+            // 
+            this.tf_login_id.Depth = 0;
+            this.tf_login_id.Hint = "ID";
+            this.tf_login_id.Location = new System.Drawing.Point(33, 88);
+            this.tf_login_id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tf_login_id.MaxLength = 32767;
+            this.tf_login_id.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tf_login_id.Name = "tf_login_id";
+            this.tf_login_id.PasswordChar = '\0';
+            this.tf_login_id.SelectedText = "";
+            this.tf_login_id.SelectionLength = 0;
+            this.tf_login_id.SelectionStart = 0;
+            this.tf_login_id.Size = new System.Drawing.Size(181, 23);
+            this.tf_login_id.TabIndex = 4;
+            this.tf_login_id.TabStop = false;
+            this.tf_login_id.UseSystemPasswordChar = false;
+            this.tf_login_id.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tf_login_id_KeyDown);
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.materialLabel2.BackColor = System.Drawing.SystemColors.Window;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.materialLabel2.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.materialLabel2.ForeColor = System.Drawing.Color.Red;
+            this.materialLabel2.Location = new System.Drawing.Point(33, 175);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(181, 19);
+            this.materialLabel2.TabIndex = 9;
+            this.materialLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 267);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.tf_login_pw);
+            this.Controls.Add(this.tf_login_id);
+            this.Name = "LoginForm";
+            this.Text = "로그인";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton RegisterButton;
+        private MaterialSkin.Controls.MaterialSingleLineTextField tf_login_pw;
+        private MaterialSkin.Controls.MaterialSingleLineTextField tf_login_id;
+        private MaterialSkin.Controls.MaterialRaisedButton SubmitButton;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+    }
+}
---
+++ client/c#/AcademyMG/MaterialSkinExample/LoginRegister/RegisterForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using RestSharp;
+using MaterialSkinExample.Encrypter;
+using System.Web;
+using Refit;
+
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+using RichardSzalay.MockHttp;
+
+using Refit.Tests;
+
+namespace MaterialSkinExample.LoginRegister
+{
+    public partial class RegisterForm : MaterialForm
+    {
+        public bool id_check_result = false;
+        public bool pw_check_result = false;
+        public bool pwcmp_check_result = false;
+        public bool name_check_result = false;
+        public bool class_check_result = true;
+
+        public class InputCheck
+        {
+            public bool flag { get; set; }
+            public string err_msg { get; set; }
+        }
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tf_register_class_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            pw_check_process();
+            pwcmp_check_process();
+            name_check_process();
+
+            if (pw_check_result && pwcmp_check_result && name_check_result && class_check_result) register_process();
+            else materialLabel6.Text = "회원가입 실패[2]";
+        }
+
+        private void tf_register_id_KeyDown(object sender, KeyEventArgs e)
+        {
+            id_check_process();
+        }
+
+        private void tf_register_id_KeyUp(object sender, KeyEventArgs e)
+        {
+            id_check_process();
+        }
+
+        private void tf_register_pw_KeyDown(object sender, KeyEventArgs e)
+        {
+            pw_check_process();
+        }
+
+        private void tf_register_pw_KeyUp(object sender, KeyEventArgs e)
+        {
+            pw_check_process();
+        }
+
+        private void tf_register_pwcmp_KeyDown(object sender, KeyEventArgs e)
+        {
+            pwcmp_check_process();
+        }
+
+        private void tf_register_pwcmp_KeyUp(object sender, KeyEventArgs e)
+        {
+            pwcmp_check_process();
+        }
+
+        private void tf_register_name_KeyDown(object sender, KeyEventArgs e)
+        {
+            name_check_process();
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void id_check_process()
+        {
+            id_check_result = false;
+            InputCheck check = register_id_check(tf_register_id.Text);
+            if (check.flag)
+            {
+                materialLabel1.Text = "";
+                var settings = new RefitSettings
+                {
+                    JsonSerializerSettings = new JsonSerializerSettings() { ContractResolver = new SnakeCasePropertyNamesContractResolver() }
+                };
+                var service = RestService.For<AcademyMG_APIs>("http://127.0.0.1:5013", settings);
+                var result = await service.IDCheck(tf_register_id.Text);
+
+                if (result.flag) materialLabel1.Text = "중복된 아이디입니다.";
+                else
+                {
+                    materialLabel1.Text = "사용가능한 아이디입니다.";
+                    id_check_result = true;
+                }
+            }
+            else materialLabel1.Text = check.err_msg;
+        }
+
+        private void pw_check_process()
+        {
+            pw_check_result = false;
+            InputCheck check = register_pw_check(tf_register_pw.Text);
+            if (check.flag)
+            {
+                materialLabel3.Text = "사용가능한 비밀번호입니다.";
+                pw_check_result = true;
+            }
+            else materialLabel3.Text = check.err_msg;
+        }
+
+        private void pwcmp_check_process()
+        {
+            pwcmp_check_result = false;
+            if (tf_register_pwcmp.Text.Equals(tf_register_pw.Text))
+            {
+                materialLabel4.Text = "비밀번호가 일치합니다.";
+                pwcmp_check_result = true;
+            }
+            else materialLabel4.Text = "비밀번호가 일치하지 않습니다.";
+        }
+
+        private void name_check_process()
+        {
+            name_check_result = false;
+            materialLabel2.Text = "사용가능한 이름입니다.";
+            name_check_result = true;
+        }
+
+        private async void register_process()
+        {
+            var settings = new RefitSettings
+            {
+                JsonSerializerSettings = new JsonSerializerSettings() { ContractResolver = new SnakeCasePropertyNamesContractResolver() }
+            };
+
+            var service = RestService.For<AcademyMG_APIs>("http://127.0.0.1:5013", settings);
+
+            var result1 = await service.IDCheck(tf_register_id.Text);
+
+            if (!result1.flag)
+            {
+                var register = new RegisterData
+                {
+                    id = tf_register_id.Text,
+                    password = SHA256_Encryptor.SHA256Hash(tf_register_pw.Text),
+                    academy_class = 1,
+                    name = tf_register_name.Text
+                };
+
+                var result = await service.Register(register);
+
+                if (result.flag == true)
+                {
+                    this.Close();
+                }
+                else materialLabel6.Text = "회원가입 실패[0]";
+            }
+            else materialLabel6.Text = "회원가입 실패[1]";
+        }
+
+        private InputCheck register_id_check(string id)
+        {
+            bool flag = false;
+            string msg = "";
+
+            if (id.Length < 4) msg = "아이디는 4글자 이상이어야 합니다.";
+            else if (id.Length >= 16) msg = "아이디는 16글자 미만이어야 합니다.";
+            else
+            {
+                foreach (char chr in id)
+                {
+                    if (!((chr >= '0' && chr <= '9') || (chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z'))) msg = "아이디는 특수문자를 포함할 수 없습니다.";
+                }
+            }
+            if (msg.Length == 0) flag = true;
+
+            return new InputCheck { flag = flag, err_msg = msg };
+        }
+
+        private InputCheck register_pw_check(string pw)
+        {
+            bool flag = false;
+            string msg = "사용가능한 비밀번호 입니다.";
+
+            if (pw.Length < 8) msg = "비밀번호는 8글자 이상이어야 합니다.";
+            else if (pw.Length >= 16) msg = "비밀번호는 16글자 미만이어야 합니다.";
+            else flag = true;
+
+            return new InputCheck { flag = flag, err_msg = msg };
+        }
+
+        
+    }
+}
